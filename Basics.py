4107f9eb4a71336d552ed0e976afee76f2a6af03
Ramana_Pallu <56391732+RamanPallu@users.noreply.github.com>
2019-10-20 23:11:20 +0530

    Inital

---
+++ Basics.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 00:39:11 2019
+
+@author: Lokesh
+"""
+var1 = 3.6
+V_464 = 32
+    
---
+++ Hello.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  8 01:02:05 2019
+
+@author: Lokesh
+"""
+
+def hello():
+    print("Hello world")
+    
+    
+    
+    
+    
+    
+    hello()
+    
